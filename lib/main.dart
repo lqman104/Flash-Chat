42a58ac64a9fcafb7b97ef6d326ed2e692c0788d
Luqman <lqman104@gmail.com>
2023-08-04 06:16:53 +0700

    refactor: change the routes name using static constant

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,12 @@ class FlashChat extends StatelessWidget {
           bodyLarge: TextStyle(color: Colors.black54),
         ),
       ),
-      initialRoute: 'welcome',
+      initialRoute: WelcomeScreen.id,
       routes: {
-        'welcome': (context) => WelcomeScreen(),
-        'login': (context) => LoginScreen(),
-        'register': (context) => RegistrationScreen(),
-        'chat': (context) => ChatScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
       },
     );
   }
